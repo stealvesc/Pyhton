9b86fab94e231f754aab07950b861f55d67e77ed
Stepane Carvalho <stealvesc@gmail.com>
2024-10-16 13:09:45 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+print ("Sabor Express\n")
+
+print ('1. Cadastrar restaurante')
+print ('2. Listrar restaurante')
+print ('3. Ativar resturante')
+print ('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print("Você escolheu a opção", opcao_escolhida)
